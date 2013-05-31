e5f89e4435288bffa6c5e6c9b243cf631a3da2ab
Derilect <derilect@hotmail.com>
2013-05-30 23:13:48 -0700

    Initial Commit

---
+++ Source/TDS/MSVC/TDS.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, L"Hi", L"Blah", MB_OK);
+	return 0;
+}
+
